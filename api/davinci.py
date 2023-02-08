6ea6904fba91ab759c207407dcb765609f19cac5
juandavidGF <davad701@gmail.com>
2023-02-08 11:36:51 -0500

    xxxx

--- api/davinci.py
+++ api/davinci.py
@@ -20,3 +20,14 @@ class handler(BaseHTTPRequestHandler):
 		self.end_headers()
 		self.wfile.write('Hello, world!'.encode('utf-8'))
 		return
+
+	# def do_POST(self):
+	# 	content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+	# 	post_data = self.rfile.read(content_length) # <--- Gets the data itself
+	# 	logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+    #             str(self.path), str(self.headers), post_data.decode('utf-8'))
+
+	# 	self._set_response()
+	# 	self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+	# 	return
+
