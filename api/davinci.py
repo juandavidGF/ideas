53025f7490e936ccfc888becd7973d045fc14951
juandavidGF <davad701@gmail.com>
2023-02-08 11:01:05 -0500

    update env variable openai

--- api/davinci.py
+++ api/davinci.py
@@ -5,7 +5,7 @@ import os
 from http.server import BaseHTTPRequestHandler
 
 # Get the API key from the environment variable
-API_KEY = os.environ['DAVINCI_API_KEY']
+API_KEY = os.environ['OPENAI_API_KEY']
 
 # Set the URL for the API request
 url = 'https://api.davinci.ai/v1/insights'
