9ebfb8e1869ef0fc7b34041f19420c48a76211ba
juandavidGF <davad701@gmail.com>
2023-02-08 08:12:18 -0500

    index jd-assistant + basic description

---
+++ script.js
@@ -0,0 +1,41 @@
+let routes = {};
+let templates = {};
+
+let app_div = document.getElementById('app');
+
+function home() {
+    let div = document.createElement('div');
+    let link = document.createElement('a');
+    link.href = '#/about';
+    link.innerText = 'About';
+
+    div.innerHTML = '<h1>Home</h1>';
+    div.appendChild(link);
+
+    app_div.appendChild(div);
+};
+
+function about() {
+    let div = document.createElement('div');
+    let link = document.createElement('a');
+    link.href = '#/';
+    link.innerText = 'Home';
+
+    div.innerHTML = '<h1>About</h1>';
+    div.appendChild(link);
+
+    app_div.appendChild(div);
+};
+
+function route (path, template) {
+	if (typeof template === 'function') {
+			return routes[path] = template;
+	}
+	else if (typeof template === 'string') {
+			return routes[path] = templates[template];
+	} else {
+			return;
+	};
+};
+
+
