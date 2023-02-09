2a78e0b9bab3dc20169bf537c4bf876438de710a
juandavidGF <davad701@gmail.com>
2023-02-09 11:55:45 -0500

    basic function in nodejs to call openai

---
+++ api/davinci.js
@@ -0,0 +1,22 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+
+const openai = new OpenAIApi(configuration);
+
+
+export default async function handler(req, res) {
+	const { body, method } = req;
+
+	if(req.method == "POST") {
+		const completion = await openai.createCompletion({
+			model: "text-davinci-002",
+			prompt: body.prompt,
+		});
+		console.log(completion.data.choices[0].text);
+		res.status(200).json({ message: completion.data.choices[0].text });
+	}
+}
