9b837fba02c00338e92c1617ec65abf1f3f4a1e0
juandavidGF <davad701@gmail.com>
2023-02-16 10:34:11 -0500

    sendgrid integration

--- src/pages/api/suscriptor.js
+++ src/pages/api/suscriptor.js
@@ -1,5 +1,29 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.API_KEY_SENDGRID);
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
+
+	if(req.method == "POST") {
+		const { email} = req.body;
+
+		const txt = `${email} have suscribed to the HN Digest newsletter`
+		const subject = `HN - fast  ${email}`;
+
+		const msg = {
+			to: 'juanchoda12@gmail.com', // Recipent
+			from: 'support@artmelon.me', // Verified sender
+			subject: subject,
+			text: txt,
+			html: `<strong>${txt}</strong>`,
+		}
+
+		try {
+			response = await sgMail.send(msg);
+		} catch (error) {
+			console.error(error);
+		}
+
+		res.status(200).json(response);
+	}
   res.status(200).json({ name: 'John Doe' })
 }
